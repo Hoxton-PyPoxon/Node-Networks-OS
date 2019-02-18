3b230a72f413afd6c1c914673de7aa860750e9b8
Hoxton <47511818+Hoxton-PyPoxon@users.noreply.github.com>
2019-02-18 12:26:25 -0600

    Add files via upload

---
+++ NodeOS/NodeOS.py
@@ -0,0 +1,30 @@
+import time
+import os
+import token
+
+NodeVersion = '1.0.0'
+
+Author = 'Hoxton'
+
+Hostname = 'E5W-Xeon'
+
+Token = 'FXj8QzJX7e6BfsNgnEEyy4BpCV69y26H'
+
+print("--{ Node Networks OS }--")
+print("---{ Version: 1.0.0 }---")
+print("")
+print("-{ Connecting to Steam... }-")
+time.sleep(5)
+print("   ")
+print(" Connection Established!")
+print("   ")
+print("-{ Loading Content files... }-")
+time.sleep(10)
+print("   ")
+print(" Content Files loaded. Preparing for startup!")
+print("   ")
+print("-{ Starting Scrds... }-")
+time.sleep(10)
+os.system(r"C:\Users\bryce\Desktop\Projects\Garrys-Mod\Server\srcds.exe -console -game garrysmod +map rp_downtown_tits_v2 +maxplayers 4 +gamemode darkrp +host_workshop_collection 1630947834 -authkey 52A0C2C035C5B90CBECFAA48BA4393A6")
+print("-----{ 0X061 - User Ended Scrds }-----")
+time.sleep(40)
